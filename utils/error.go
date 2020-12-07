9df4cf79cfbf85f5ba831481c6f4973f98205388
Jayce <jaycelau@163.com>
2020-12-07 16:27:59 +0800

    feat: error
    add error with exit code 1

--- utils/error.go
+++ utils/error.go
@@ -1,17 +1,20 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
-func PanicWhenErr(err error)  {
-	if err!=nil {
+func PanicWhenErr(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
 
 func LogWhenErr(err error) {
 	if err != nil {
-		log.Error().Msg(err.Error())
+		log.Error().Msg(fmt.Sprintf("exit with error: %s", err.Error()))
+		os.Exit(1)
 	}
 }
